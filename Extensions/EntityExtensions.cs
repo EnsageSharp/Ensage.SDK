881bbc576d6fd99c044e558830f438f4574f33b7
AlexD3v <alexdev@tutanota.com>
2017-03-31 18:34:37 +0200

    add more extensions

--- Extensions/EntityExtensions.cs
+++ Extensions/EntityExtensions.cs
@@ -12,9 +12,9 @@ namespace Ensage.SDK.Extensions
 
     public static class EntityExtensions
     {
-        public static float Distance2D(this Unit entity, Unit other)
+        public static float Distance2D(this Unit entity, Unit other, bool fromCenterToCenter = false)
         {
-            return entity.Distance2D(other.NetworkPosition) - entity.HullRadius - other.HullRadius;
+            return entity.Distance2D(other.NetworkPosition) - (fromCenterToCenter ? 0f : (entity.HullRadius + other.HullRadius));
         }
 
         public static float Distance2D(this Entity entity, Entity other)
@@ -35,6 +35,42 @@ namespace Ensage.SDK.Extensions
             return
                 (float)
                 Math.Sqrt(Math.Pow(entityPosition.X - position.X, 2) + Math.Pow(entityPosition.Y - position.Y, 2));
+        }
+
+        /// <summary>
+        /// Returns if the distance to target is lower than range
+        /// </summary>
+        /// <param name="sourcePosition"></param>
+        /// <param name="target"></param>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public static bool IsInRange(this Entity source, Entity target, float range)
+        {
+            return source.NetworkPosition.IsInRange(target, range);
+        }
+
+        /// <summary>
+        /// Returns if the distance to target is lower than range
+        /// </summary>
+        /// <param name="sourcePosition"></param>
+        /// <param name="target"></param>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public static bool IsInRange(this Entity source, Vector2 targetPosition, float range)
+        {
+            return source.NetworkPosition.IsInRange(targetPosition, range);
+        }
+
+        /// <summary>
+        /// Returns if the distance to target is lower than range
+        /// </summary>
+        /// <param name="sourcePosition"></param>
+        /// <param name="target"></param>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public static bool IsInRange(this Entity source, Vector3 targetPosition, float range)
+        {
+            return source.NetworkPosition.IsInRange(targetPosition, range);
         }
     }
 }
--- Extensions/UnitExtensions.cs
+++ Extensions/UnitExtensions.cs
@@ -5,6 +5,7 @@
 namespace Ensage.SDK.Extensions
 {
     using System;
+    using System.Collections.Generic;	
     using System.Linq;
 
     using SharpDX;
@@ -16,7 +17,7 @@ namespace Ensage.SDK.Extensions
             try
             {
                 var attackAnimationPoint =
-                    Game.FindKeyValues($"{unit.Name}/AttackAnimationPoint", KeyValueSource.Unit).FloatValue;
+                    Game.FindKeyValues($"{unit.Name}/AttackAnimationPoint", unit is Hero ? KeyValueSource.Hero : KeyValueSource.Unit).FloatValue;
 
                 return attackAnimationPoint / (1 + (unit.AttackSpeedValue() - 100) / 100);
             }
@@ -26,49 +27,79 @@ namespace Ensage.SDK.Extensions
             }
         }
 
-        public static float AttackRange(this Unit unit)
+        public static float AttackRange(this Unit unit, Unit target = null)
         {
-            var result = (float)unit.AttackRange;
+            var result = (float)(unit.AttackRange + unit.HullRadius);
 
-            // test for items with bonus range
-            var bonusRangeItem = unit.GetItemById(AbilityId.item_dragon_lance)
-                                 ?? unit.GetItemById(AbilityId.item_hurricane_pike);
-            if (bonusRangeItem != null)
+            if (target != null)
             {
-                result += bonusRangeItem.GetAbilitySpecialData("base_attack_range");
+                result += target.HullRadius;
             }
 
-            // test for abilities with bonus range
-            var sniperTakeAim = unit.GetAbilityById(AbilityId.sniper_take_aim);
-            if (sniperTakeAim != null && sniperTakeAim.Level > 0)
+            if(unit is Creep)
             {
-                result += sniperTakeAim.GetAbilitySpecialData("bonus_attack_range");
+                result += 15f;
             }
 
-            var psiBlades = unit.GetAbilityById(AbilityId.templar_assassin_psi_blades);
-            if (sniperTakeAim != null && sniperTakeAim.Level > 0)
+            if(unit is Hero)
             {
-                result += psiBlades.GetAbilitySpecialData("bonus_attack_range");
-            }
-
-            // test for talents with bonus range
-            foreach (var ability in unit.Spellbook.Spells.Where(x => x.Name.StartsWith("special_bonus_attack_range_")))
-            {
-                if (ability.Level > 0)
+                // test for items with bonus range
+                var bonusRangeItem = unit.GetItemById(AbilityId.item_dragon_lance)
+                                     ?? unit.GetItemById(AbilityId.item_hurricane_pike);
+                if (bonusRangeItem != null)
                 {
-                    result += ability.GetAbilitySpecialData("value");
+                    result += bonusRangeItem.GetAbilitySpecialData("base_attack_range");
+                }
+
+                // test for abilities with bonus range
+                var sniperTakeAim = unit.GetAbilityById(AbilityId.sniper_take_aim);
+                if (sniperTakeAim != null && sniperTakeAim.Level > 0)
+                {
+                    result += sniperTakeAim.GetAbilitySpecialData("bonus_attack_range");
+                }
+
+                var psiBlades = unit.GetAbilityById(AbilityId.templar_assassin_psi_blades);
+                if (psiBlades != null && psiBlades.Level > 0)
+                {
+                    result += psiBlades.GetAbilitySpecialData("bonus_attack_range");
+                }
+
+                // test for talents with bonus range
+                foreach (var ability in unit.Spellbook.Spells.Where(x => x.Name.StartsWith("special_bonus_attack_range_")))
+                {
+                    if (ability.Level > 0)
+                    {
+                        result += ability.GetAbilitySpecialData("value");
+                    }
                 }
             }
-
+            
             // test for modifiers with bonus range TODO
             return result;
         }
 
-        public static float AttackSpeedValue(this Unit hero)
+        public static float AttackSpeedValue(this Unit unit)
         {
             // TODO modifiers like ursa overpower
-            var attackSpeed = Math.Max(20, hero.AttackSpeedValue);
+            var attackSpeed = Math.Max(20, unit.AttackSpeedValue);
             return Math.Min(attackSpeed, 600);
+        }
+
+        public static bool CanAttack(this Unit unit)
+        {
+            return unit.AttackCapability != AttackCapability.None && !unit.IsDisarmed();
+        }
+
+        public static float FindRotationAngle(this Unit unit, Vector3 pos)
+        {
+            var angle = Math.Abs(Math.Atan2(pos.Y - unit.Position.Y, pos.X - unit.Position.X) - unit.RotationRad);
+
+            if (angle > Math.PI)
+            {
+                angle = Math.PI * 2 - angle;
+            }
+
+            return (float)angle;
         }
 
         public static Ability GetAbilityById(this Unit unit, AbilityId abilityId)
@@ -76,36 +107,194 @@ namespace Ensage.SDK.Extensions
             return unit.Spellbook.Spells.FirstOrDefault(x => x.Id == 0);
         }
 
-        public static float GetAttackDamage(this Unit unit, Unit target)
+        /// <summary>
+        /// Returns the damage an auto-attack would do to the target
+        /// </summary>
+        /// <param name="source">The attacker</param>
+        /// <param name="target">The target</param>
+        /// <returns></returns>
+        public static float GetAttackDamage(this Unit source, Unit target, bool useMinimumDamage = false)
         {
-            var damage = (float)unit.MinimumDamage + unit.BonusDamage;
+            float damage = (!useMinimumDamage ? source.DamageAverage : source.MinimumDamage) + source.BonusDamage;
+            var mult = 1f;
+            var damageType = source.AttackDamageType;
+            var armorType = target.ArmorType;
+
+            if (damageType == AttackDamageType.Hero && armorType == ArmorType.Structure)
+            {
+                mult *= .5f;
+            }
+            else if (damageType == AttackDamageType.Basic && armorType == ArmorType.Hero)
+            {
+                mult *= .75f;
+            }
+            else if (damageType == AttackDamageType.Basic && armorType == ArmorType.Structure)
+            {
+                mult *= .7f;
+            }
+            else if (damageType == AttackDamageType.Pierce && armorType == ArmorType.Hero)
+            {
+                mult *= .5f;
+            }
+            else if (damageType == AttackDamageType.Pierce && armorType == ArmorType.Basic)
+            {
+                mult *= 1.5f;
+            }
+            else if (damageType == AttackDamageType.Pierce && armorType == ArmorType.Structure)
+            {
+                mult *= .35f;
+            }
+            else if (damageType == AttackDamageType.Siege && armorType == ArmorType.Hero)
+            {
+                mult *= .85f;
+            }
+            else if (damageType == AttackDamageType.Siege && armorType == ArmorType.Structure)
+            {
+                mult *= 1.50f;
+            }
 
             if (target.IsNeutral || target is Creep)
             {
                 // TODO test IsNeutral -> quelling blade bonus applied?
-                var isMelee = unit.IsMelee;
-                var quellingBlade = unit.GetItemById(AbilityId.item_quelling_blade)
-                                    ?? unit.GetItemById(AbilityId.item_iron_talon);
+                var isMelee = source.IsMelee;
+                var quellingBlade = source.GetItemById(AbilityId.item_quelling_blade)
+                                    ?? source.GetItemById(AbilityId.item_iron_talon);
                 if (quellingBlade != null)
                 {
                     damage += quellingBlade.GetAbilitySpecialData(isMelee ? "damage_bonus" : "damage_bonus_ranged");
                 }
             }
 
-            // TODO: desolator -armor modifier
-            damage *= 1.0f - target.DamageResist;
-            return damage;
+            var armor = target.Armor;
+
+            mult *= (1 - 0.06f * armor / (1 + 0.06f * Math.Abs(armor)));
+
+            return damage * mult;
+        }
+
+        /// <summary>
+        /// Returns the amount of time that it would take for the auto-attack to hit the target
+        /// </summary>
+        /// <param name="source">The attacker</param>
+        /// <param name="target">The target</param>
+        /// <returns></returns>
+        public static float GetAutoAttackArrivalTime(this Unit source, Unit target, bool takeRotationTimeIntoAccount = true)
+        {
+            var result = GetProjectileArrivalTime(source, target, source.AttackPoint(), source.IsMelee ? float.MaxValue : source.ProjectileSpeed(), takeRotationTimeIntoAccount);
+
+            if (!(source is Tower))
+            {
+                result -= 0.05f; //:broscience:
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Returns the amount of time that it would take for a missile to hit the target
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        /// <param name="delay"></param>
+        /// <param name="missileSpeed"></param>
+        /// <param name="takeRotationIntoAccount"></param>
+        /// <returns></returns>
+        public static float GetProjectileArrivalTime(this Unit source, Unit target, float delay, float missileSpeed, bool takeRotationTimeIntoAccount = true)
+        {
+            var result = 0f;
+
+            //rotation time
+            result += takeRotationTimeIntoAccount ? source.TurnTime(target.NetworkPosition) : 0f;
+
+            //delay
+            result += delay;
+
+            //time that takes to the missile to reach the target
+            if (missileSpeed != float.MaxValue)
+            {
+                result += source.Distance2D(target) / missileSpeed;
+            }
+
+            return result;
         }
 
         public static Item GetItemById(this Unit unit, AbilityId abilityId)
         {
-            return unit.Inventory.Items.FirstOrDefault(x => x.Id == abilityId);
+            if(!unit.HasInventory)
+            {
+                return null;
+            }
+            return unit.Inventory.Items.FirstOrDefault(x => x != null && x.IsValid && x.Id == abilityId);
+        }
+
+        public static bool HasModifier(this Unit unit, string modifierName)
+        {
+            return unit.Modifiers.Any(modifier => modifier.Name == modifierName);
+        }
+
+        public static bool HasModifiers(this Unit unit, IEnumerable<string> modifierNames, bool hasAll = true)
+        {
+            var counter = 0;
+            
+            foreach (var modifier in unit.Modifiers)
+            {
+                if(modifierNames.Contains(modifier.Name))
+                {
+                    counter++;
+                    if(hasAll)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return hasAll ? counter == modifierNames.Count() : false;
         }
 
         public static Vector3 InFront(this Unit unit, float distance)
         {
             var v = unit.Position + unit.Vector3FromPolarAngle() * distance;
             return new Vector3(v.X, v.Y, 0);
+        }
+
+        /// <summary>
+        /// returns true if source is directly facing to target
+        /// </summary>
+        /// <param name="source"></param>
+        /// <param name="target"></param>
+        /// <returns></returns>
+        public static bool IsDirectlyFacing(this Unit source, Unit target)
+        {
+            var vector1 = target.NetworkPosition - source.NetworkPosition;
+            var diff = Math.Abs(Math.Atan2(vector1.Y, vector1.X) - source.RotationRad);
+            return diff < 0.025f;
+        }
+
+        public static bool IsDisarmed(this Unit unit)
+        {
+            return unit.UnitState.HasFlag(UnitState.Disarmed);
+        }
+
+        public static bool IsInAttackRange(this Unit source, Unit target)
+        {
+            return source.IsInRange(target, source.AttackRange(target), true);
+        }
+
+        /// <summary>
+        /// Returns if the distance to target is lower than range
+        /// </summary>
+        /// <param name="sourcePosition"></param>
+        /// <param name="target"></param>
+        /// <param name="range"></param>
+        /// <returns></returns>
+        public static bool IsInRange(this Unit source, Unit target, float range, bool centerToCenter = false)
+        {
+            return source.NetworkPosition.IsInRange(target, centerToCenter ? range : Math.Max(0, range - source.HullRadius - target.HullRadius));
+        }
+
+        public static bool IsInvulnerable(this Unit unit)
+        {
+            return unit.UnitState.HasFlag(UnitState.Invulnerable);
         }
 
         public static bool IsMagicImmune(this Unit unit)
@@ -126,6 +315,87 @@ namespace Ensage.SDK.Extensions
         public static bool IsSilenced(this Unit unit)
         {
             return unit.UnitState.HasFlag(UnitState.Silenced);
+        }
+
+        public static bool IsStunned(this Unit unit)
+        {
+            return unit.UnitState.HasFlag(UnitState.Stunned);
+        }
+
+        public static bool IsValidOrbwalkingTarget(this Unit attacker, Unit target)
+        {
+            return target.IsValid && target.IsVisible && target.IsAlive && target.IsSpawned && !target.IsIllusion &&
+                   attacker.IsInAttackRange(target) && !target.IsInvulnerable();
+        }
+
+        /// <summary>
+        /// Returns the units auto attacks projectile speed
+        /// </summary>
+        /// <param name="unit"></param>
+        /// <returns></returns>
+        public static float ProjectileSpeed(this Unit unit)
+        {
+            try
+            {
+                return Game.FindKeyValues($"{unit.Name}/ProjectileSpeed", unit is Hero ? KeyValueSource.Hero : KeyValueSource.Unit).IntValue;
+            }
+            catch (KeyValuesNotFoundException)
+            {
+                return 0;
+            }
+        }
+
+        public static float TurnRate(this Unit unit, bool currentTurnRate = true)
+        {
+            try
+            {
+                var turnRate =
+                    Game.FindKeyValues($"{unit.Name}/MovementTurnRate", unit is Hero ? KeyValueSource.Hero : KeyValueSource.Unit).FloatValue;
+
+                if(currentTurnRate)
+                {
+                    if (unit.HasModifier("modifier_medusa_stone_gaze_slow"))
+                    {
+                        turnRate *= 0.65f;
+                    }
+
+                    if (unit.HasModifier("modifier_batrider_sticky_napalm"))
+                    {
+                        turnRate *= 0.3f;
+                    }
+                }
+
+                return turnRate;
+            }
+            catch (KeyValuesNotFoundException)
+            {
+                return 0.5f;
+            }
+        }
+
+        public static float TurnTime(this Unit unit, Vector3 position)
+        {
+            return TurnTime(unit, unit.FindRotationAngle(position));
+        }
+
+        public static float TurnTime(this Unit unit, Vector2 position)
+        {
+            return TurnTime(unit, unit.FindRotationAngle(position.ToVector3()));
+        }
+
+        public static float TurnTime(this Unit unit, float angle)
+        {
+            if (unit.ClassId == ClassId.CDOTA_Unit_Hero_Wisp)
+            {
+                return 0;
+            }
+
+            if (angle <= 0.5f)
+            {
+                return 0;
+            }
+
+            return 0.03f / unit.TurnRate(unit is Hero) * angle;
         }
 
         public static Vector2 Vector2FromPolarAngle(this Unit unit, float delta = 0f, float radial = 1f)

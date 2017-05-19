172e3c14318e27d215f9f820f60d1cdd975c4d6f
h3h3 <h3h3sharp@gmail.com>
2017-05-19 03:45:30 +0200

    added DummyTargetSelector

---
+++ TargetSelector/Modes/DummyTargetSelector.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="DummyTargetSelector.cs" company="Ensage">
+//    Copyright (c) 2017 Ensage.
+// </copyright>
+
+namespace Ensage.SDK.TargetSelector.Modes
+{
+    using System.Collections.Generic;
+    using System.ComponentModel.Composition;
+    using System.Linq;
+
+    using Ensage.Common.Extensions;
+    using Ensage.SDK.Helpers;
+    using Ensage.SDK.Service;
+    using Ensage.SDK.TargetSelector.Metadata;
+
+    [ExportTargetSelector("Target Dummy")]
+    public class DummyTargetSelector : ControllableService, ITargetSelector
+    {
+        [ImportingConstructor]
+        public DummyTargetSelector([Import] IServiceContext context)
+        {
+            this.Owner = context.Owner;
+        }
+
+        private Hero Owner { get; }
+
+        public IEnumerable<Unit> GetTargets()
+        {
+            return EntityManager<Unit>.Entities
+                                      .Where(e => e.ClassId == ClassId.CDOTA_Unit_TargetDummy)
+                                      .OrderBy(e => e.Distance2D(this.Owner));
+        }
+    }
+}

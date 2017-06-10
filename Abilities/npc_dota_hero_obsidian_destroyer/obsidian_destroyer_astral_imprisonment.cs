a842251bff621b895ae29bd65d9ecd5d98b58999
beminee <emirdota2@gmail.com>
2017-06-10 23:11:10 +0300

    2 more spells
    2 more spells

---
+++ Abilities/npc_dota_hero_obsidian_destroyer/obsidian_destroyer_astral_imprisonment.cs
@@ -0,0 +1,35 @@
+﻿
+namespace Ensage.SDK.Abilities.npc_dota_hero_obsidian_destroyer
+{
+
+    using Ensage.SDK.Extensions;
+
+    using Ensage.SDK.Helpers;
+
+
+    public class obsidian_destroyer_astral_imprisonment : RangedAbility, IHasTargetModifier, IHasModifier
+    {
+        public obsidian_destroyer_astral_imprisonment(Ability ability)
+            : base(ability)
+        {
+        }
+
+        public string ModifierName => "modifier_obsidian_destroyer_astral_imprisonment_buff";
+        public string TargetModifierName => "modifier_obsidian_destroyer_astral_imprisonment_debuff";
+
+        public override float GetDamage(params Unit[] targets)
+        {
+            var totalDamage = 0.0f;
+
+            var damage = this.GetRawDamage();
+            var amplify = this.Owner.GetSpellAmplification();
+            foreach (var target in targets)
+            {
+                var reduction = this.Ability.GetDamageReduction(target);
+                totalDamage += DamageHelpers.GetSpellDamage(damage, amplify, reduction);
+            }
+
+            return totalDamage;
+        }
+    }
+}

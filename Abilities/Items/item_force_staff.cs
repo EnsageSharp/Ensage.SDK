d8dd3828b1c7e4bed24d22bb321f72010ac47899
Rulfy <git@zynox.net>
2017-06-20 12:30:33 +0200

    added forcestaff

---
+++ Abilities/Items/item_force_staff.cs
@@ -0,0 +1,25 @@
+﻿// <copyright file="item_force_staff.cs" company="Ensage">
+//    Copyright (c) 2017 Ensage.
+// </copyright>
+namespace Ensage.SDK.Abilities.Items
+{
+    using Ensage.SDK.Extensions;
+
+    public class item_force_staff : RangedAbility, IHasTargetModifier
+    {
+        public item_force_staff(Item item)
+            : base(item)
+        {
+        }
+
+        public string TargetModifierName { get; } = "modifier_item_forcestaff_active";
+
+        public float PushLength
+        {
+            get
+            {
+                return this.Ability.GetAbilitySpecialData("push_length");
+            }
+        }
+    }
+}

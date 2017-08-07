17d5f720a79937d5c0892867d57b0a98731b3c98
IdcNoob <zajabli@gmail.com>
2017-08-08 02:56:33 +0300

    Puck abilities

---
+++ Abilities/npc_dota_hero_puck/puck_dream_coil.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="puck_dream_coil.cs" company="Ensage">
+//    Copyright (c) 2017 Ensage.
+// </copyright>
+
+namespace Ensage.SDK.Abilities.npc_dota_hero_puck
+{
+    using Ensage.SDK.Extensions;
+    using Ensage.SDK.Helpers;
+
+    public class puck_dream_coil : CircleAbility, IAreaOfEffectAbility, IHasTargetModifier
+    {
+        public puck_dream_coil(Ability ability)
+            : base(ability)
+        {
+        }
+
+        public string TargetModifierName { get; } = "modifier_puck_coiled";
+
+        protected override float RawDamage
+        {
+            get
+            {
+                return this.Ability.GetAbilitySpecialData("coil_init_damage_tooltip");
+            }
+        }
+
+        public override float GetDamage(params Unit[] targets)
+        {
+            var totalDamage = 0.0f;
+
+            var damage = this.RawDamage;
+            var amplify = this.Owner.GetSpellAmplification();
+            foreach (var target in targets)
+            {
+                var reduction = this.Ability.GetDamageReduction(target);
+                totalDamage += DamageHelpers.GetSpellDamage(damage, amplify, reduction);
+            }
+
+            return totalDamage;
+        }
+    }
+}
---
+++ Abilities/npc_dota_hero_puck/puck_waning_rift.cs
@@ -0,0 +1,42 @@
+﻿// <copyright file="puck_waning_rift.cs" company="Ensage">
+//    Copyright (c) 2017 Ensage.
+// </copyright>
+
+namespace Ensage.SDK.Abilities.npc_dota_hero_puck
+{
+    using Ensage.SDK.Extensions;
+    using Ensage.SDK.Helpers;
+
+    public class puck_waning_rift : ActiveAbility, IAreaOfEffectAbility, IHasTargetModifierTexture
+    {
+        public puck_waning_rift(Ability ability)
+            : base(ability)
+        {
+        }
+
+        public float Radius
+        {
+            get
+            {
+                return this.Ability.GetAbilitySpecialData("radius");
+            }
+        }
+
+        public string[] TargetModifierTextureName { get; } = { "puck_waning_rift" };
+
+        public override float GetDamage(params Unit[] targets)
+        {
+            var totalDamage = 0.0f;
+
+            var damage = this.RawDamage;
+            var amplify = this.Owner.GetSpellAmplification();
+            foreach (var target in targets)
+            {
+                var reduction = this.Ability.GetDamageReduction(target);
+                totalDamage += DamageHelpers.GetSpellDamage(damage, amplify, reduction);
+            }
+
+            return totalDamage;
+        }
+    }
+}

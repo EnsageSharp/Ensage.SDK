e95d5ec4b91d63ef5a8453413292f2fe4ad16b09
beminee <emirdota2@gmail.com>
2017-08-30 22:10:36 +0300

    Added Travel Boots properly

--- Abilities/Items/item_travel_boots_2.cs
+++ Abilities/Items/item_travel_boots_2.cs
@@ -4,50 +4,12 @@
 
 namespace Ensage.SDK.Abilities.Items
 {
-    public class item_travel_boots_2 : RangedAbility, IChannable, IHasModifier, IHasModifierTexture
+    public class item_travel_boots_2 : TravelBoots, IHasModifierTexture
     {
         public item_travel_boots_2(Item item)
             : base(item)
         {
         }
-
-        public float Duration
-        {
-            get
-            {
-                var level = this.Ability.Level;
-                if (level == 0)
-                {
-                    return 0;
-                }
-
-                return this.Ability.GetChannelTime(level - 1);
-            }
-        }
-
-        public bool IsChanneling
-        {
-            get
-            {
-                return this.Ability.IsChanneling;
-            }
-        }
-
-        public string ModifierName { get; } = "modifier_teleporting";
-
         public string[] ModifierTextureName { get; } = { "item_travel_boots_2" };
-
-        public float RemainingDuration
-        {
-            get
-            {
-                if (!this.IsChanneling)
-                {
-                    return 0;
-                }
-
-                return this.Duration - this.Ability.ChannelTime;
-            }
-        }
     }
 }

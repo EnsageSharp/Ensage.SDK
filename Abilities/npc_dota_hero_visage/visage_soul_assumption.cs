7e4a16524694783cfb1eac1a2d969725bcec6c75
osmanbonus007 <osmanbonus007@mail.ru>
2017-09-01 00:23:00 +0300

    Added Visage Abilities

---
+++ Abilities/npc_dota_hero_visage/visage_soul_assumption.cs
@@ -0,0 +1,41 @@
+﻿// <copyright file="visage_soul_assumption.cs" company="Ensage">
+//    Copyright (c) 2017 Ensage.
+// </copyright>
+
+namespace Ensage.SDK.Abilities.npc_dota_hero_visage
+{
+    using System.Linq;
+
+    using Ensage.SDK.Extensions;
+
+    public class visage_soul_assumption : RangedAbility, IHasModifier
+    {
+        public visage_soul_assumption(Ability ability)
+            : base(ability)
+        {
+        }
+
+        public string ModifierName { get; } = "modifier_visage_soul_assumption";
+
+        public float StackCount
+        {
+            get
+            {
+                return this.Owner.Modifiers.FirstOrDefault(x => x.Name == "modifier_visage_soul_assumption").StackCount;
+            }
+        }
+
+        protected override float RawDamage
+        {
+            get
+            {
+                var damage = this.Ability.GetAbilitySpecialData("soul_base_damage");
+
+                var bonus = this.Ability.GetAbilitySpecialData("soul_charge_damage");
+                damage += StackCount * bonus;
+
+                return damage;
+            }
+        }
+    }
+}
---
+++ Abilities/npc_dota_hero_visage/visage_summon_familiars.cs
@@ -0,0 +1,14 @@
+// <copyright file="visage_summon_familiars.cs" company="Ensage">
+//    Copyright (c) 2017 Ensage.
+// </copyright>
+
+namespace Ensage.SDK.Abilities.npc_dota_hero_visage
+{
+    public class visage_summon_familiars : ActiveAbility
+    {
+        public visage_summon_familiars(Ability ability)
+            : base(ability)
+        {
+        }
+    }
+}

9e0b33051bf1450ed7f8f7249d10093c79eb1b5e
osmanbonus007 <osmanbonus007@mail.ru>
2017-09-01 09:48:16 +0300

    Added Magnus Abilities

---
+++ Abilities/npc_dota_hero_magnataur/magnataur_shockwave.cs
@@ -0,0 +1,34 @@
+﻿// <copyright file="magnataur_shockwave.cs" company="Ensage">
+//    Copyright (c) 2017 Ensage.
+// </copyright>
+
+namespace Ensage.SDK.Abilities.npc_dota_hero_magnataur
+{
+    using Ensage.SDK.Extensions;
+
+    public class magnataur_shockwave : LineAbility
+    {
+        public magnataur_shockwave(Ability ability)
+            : base(ability)
+        {
+        }
+
+        protected override string RadiusName { get; } = "shock_width";
+
+        protected override string SpeedName
+        {
+            get
+            {
+                return this.Owner.HasAghanimsScepter() ? "scepter_speed" : "shock_speed";
+            }
+        }
+
+        protected override float RawDamage
+        {
+            get
+            {
+                return this.Ability.GetAbilitySpecialData("shock_damage");
+            }
+        }
+    }
+}
---
+++ Abilities/npc_dota_hero_magnataur/magnataur_skewer.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="magnataur_skewer.cs" company="Ensage">
+//    Copyright (c) 2017 Ensage.
+// </copyright>
+
+namespace Ensage.SDK.Abilities.npc_dota_hero_magnataur
+{
+    using Ensage.SDK.Extensions;
+
+    public class magnataur_skewer : LineAbility, IHasTargetModifier, IHasModifier
+    {
+        public magnataur_skewer(Ability ability)
+            : base(ability)
+        {
+        }
+
+        protected override string RadiusName { get; } = "skewer_radius";
+
+        protected override string SpeedName { get; } = "skewer_speed";
+
+        protected override float RawDamage
+        {
+            get
+            {
+                return this.Ability.GetAbilitySpecialData("skewer_damage");
+            }
+        }
+
+        public string ModifierName { get; } = "modifier_magnataur_skewer_movement";
+
+        public string TargetModifierName { get; } = "modifier_magnataur_skewer_impact";
+    }
+}

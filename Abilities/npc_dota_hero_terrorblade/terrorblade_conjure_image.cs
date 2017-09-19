09e96261661e23dd2be47f20b9dd4e80a410b58e
osmanbonus007 <osmanbonus007@mail.ru>
2017-09-19 14:41:05 +0300

    Added Terrorblade Abilities

---
+++ Abilities/npc_dota_hero_terrorblade/terrorblade_conjure_image.cs
@@ -0,0 +1,14 @@
+// <copyright file="terrorblade_conjure_image.cs" company="Ensage">
+//    Copyright (c) 2017 Ensage.
+// </copyright>
+
+namespace Ensage.SDK.Abilities.npc_dota_hero_terrorblade
+{
+    public class terrorblade_conjure_image : ActiveAbility
+    {
+        public terrorblade_conjure_image(Ability ability)
+            : base(ability)
+        {
+        }
+    }
+}

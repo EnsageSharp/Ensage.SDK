7bc6e4318856237e18fa8ea8a0dab50e8f45e285
osmanbonus007 <osmanbonus007@mail.ru>
2017-10-06 11:00:40 +0300

    Added Damage Talent Zeus Arc Lightning

--- Abilities/npc_dota_hero_zuus/zuus_arc_lightning.cs
+++ Abilities/npc_dota_hero_zuus/zuus_arc_lightning.cs
@@ -10,5 +10,20 @@ namespace Ensage.SDK.Abilities.npc_dota_hero_zuus
             : base(ability)
         {
         }
+        
+        protected override float RawDamage
+        {
+            get
+            {
+                var damage = this.Ability.GetAbilitySpecialData("#AbilityDamage");
+                var talent = this.Owner.GetAbilityById(AbilityId.special_bonus_unique_zeus_2);
+                if (talent != null && talent.Level > 0)
+                {
+                    damage += talent.GetAbilitySpecialData("value");
+                }
+
+                return damage;
+            }
+        }
     }
 }

16cd80bc52f45794612d3a6b862cacfaf4badb70
osmanbonus007 <osmanbonus007@mail.ru>
2017-10-17 01:52:16 +0300

    Improved Ability Skywrath Concussive Shot

--- Abilities/npc_dota_hero_skywrath_mage/skywrath_mage_concussive_shot.cs
+++ Abilities/npc_dota_hero_skywrath_mage/skywrath_mage_concussive_shot.cs
@@ -4,8 +4,13 @@
 
 namespace Ensage.SDK.Abilities.npc_dota_hero_skywrath_mage
 {
+    using System.Linq;
+
     using Ensage.SDK.Abilities.Components;
     using Ensage.SDK.Extensions;
+    using Ensage.SDK.Helpers;
+
+    using PlaySharp.Toolkit.Helper.Annotations;
 
     public class skywrath_mage_concussive_shot : ActiveAbility, IAreaOfEffectAbility, IHasTargetModifier
     {
@@ -18,7 +23,7 @@ namespace Ensage.SDK.Abilities.npc_dota_hero_skywrath_mage
         {
             get
             {
-                return this.Ability.GetAbilitySpecialData("launch_radius");
+                return this.Ability.GetAbilitySpecialData("launch_radius") - 25f;  // bug fixed
             }
         }
 
@@ -38,6 +43,59 @@ namespace Ensage.SDK.Abilities.npc_dota_hero_skywrath_mage
             {
                 return this.Ability.GetAbilitySpecialData("damage");
             }
+        }
+
+        public Hero TargetHit
+        {
+            get
+            {
+                return EntityManager<Hero>.Entities.OrderBy(x => 
+                              x.Distance2D(this.Owner)).FirstOrDefault(x =>
+                                    x.IsAlive &&
+                                    x.IsVisible &&
+                                    !x.IsIllusion &&
+                                    x.IsValid &&
+                                    x.IsEnemy(Owner) &&
+                                    x.Distance2D(Owner) <= this.Radius);
+            }
+        }
+
+        public override float GetDamage(params Unit[] targets)
+        {
+            if (!targets.Any(x => x == TargetHit))
+            {
+                return 0;
+            }
+
+            var totalDamage = 0.0f;
+
+            var damage = this.RawDamage;
+            var amplify = this.Owner.GetSpellAmplification();
+            foreach (var target in targets)
+            {
+                var reduction = this.Ability.GetDamageReduction(target, this.DamageType);
+                totalDamage += DamageHelpers.GetSpellDamage(damage, amplify, reduction);
+            }
+
+            return totalDamage;
+        }
+
+        public override float GetDamage([NotNull] Unit target, float damageModifier, float targetHealth = float.MinValue)
+        {
+            if (TargetHit != target)
+            {
+                return 0;
+            }
+
+            var amplify = this.Owner.GetSpellAmplification();
+            var reduction = this.Ability.GetDamageReduction(target, this.DamageType);
+
+            return DamageHelpers.GetSpellDamage(this.RawDamage, amplify, -reduction, damageModifier);
+        }
+
+        public override bool CanHit(params Unit[] targets)
+        {
+            return targets.All(x => x.Distance2D(this.Owner) < this.Radius);
         }
     }
 }

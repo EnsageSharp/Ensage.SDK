43f3ddaa4a17362edfdc4b45fc70f4c299f839a3
YEEEEEEE777 <33149580+YEEEEEEE777@users.noreply.github.com>
2017-11-01 15:30:36 +0300

    Improved Ability Lightning Bolt

--- Abilities/npc_dota_hero_zuus/zuus_lightning_bolt.cs
+++ Abilities/npc_dota_hero_zuus/zuus_lightning_bolt.cs
@@ -5,11 +5,17 @@
 namespace Ensage.SDK.Abilities.npc_dota_hero_zuus
 {
     using System.Linq;
+    using System.Reflection;
 
     using Ensage.SDK.Extensions;
 
+    using log4net;
+    using PlaySharp.Toolkit.Logging;
+
     public class zuus_lightning_bolt : RangedAbility, IAreaOfEffectAbility
     {
+        private static readonly ILog Log = AssemblyLogs.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+
         private readonly zuus_static_field staticField;
 
         public zuus_lightning_bolt(Ability ability)
@@ -20,6 +26,36 @@ namespace Ensage.SDK.Abilities.npc_dota_hero_zuus
             {
                 this.staticField = new zuus_static_field(passive);
             }
+        }
+
+        public bool UseAbility(Unit target, bool onlyTarget = false)
+        {
+            if (!this.CanBeCasted)
+            {
+                Log.Debug($"blocked {this}");
+                return false;
+            }
+
+            bool result;
+            if ((this.Ability.AbilityBehavior & AbilityBehavior.NoTarget) == AbilityBehavior.NoTarget)
+            {
+                result = this.Ability.UseAbility();
+            }
+            else if ((this.Ability.AbilityBehavior & AbilityBehavior.Point) == AbilityBehavior.Point && !onlyTarget)
+            {
+                result = this.Ability.UseAbility(target.NetworkPosition);
+            }
+            else
+            {
+                result = this.Ability.UseAbility(target);
+            }
+
+            if (result)
+            {
+                this.LastCastAttempt = Game.RawGameTime;
+            }
+
+            return result;
         }
 
         public float Radius

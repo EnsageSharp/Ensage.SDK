9e12858c83cf59b29b3532a97fc404a6dc5e0e8e
Rulfy <git@zynox.net>
2017-12-12 04:38:26 +0100

    fixed D3D9Renderer.DrawTexture

--- Renderer/DX9/D3D9Renderer.cs
+++ Renderer/DX9/D3D9Renderer.cs
@@ -9,7 +9,6 @@ namespace Ensage.SDK.Renderer.DX9
     using System.ComponentModel.Composition;
 
     using Ensage.Common.Extensions;
-    using Ensage.SDK.Renderer.DX11;
     using Ensage.SDK.Renderer.Metadata;
 
     using SharpDX;
@@ -39,7 +38,6 @@ namespace Ensage.SDK.Renderer.DX9
             this.context = context;
             this.fontCache = fontCache;
             this.textureManager = textureManager;
-
             this.line = new Line(this.context.Device)
                             {
                                 Antialias = true
@@ -67,42 +65,6 @@ namespace Ensage.SDK.Renderer.DX9
         {
             this.Dispose(true);
             GC.SuppressFinalize(this);
-        }
-
-        public void DrawTexture(string textureKey, RectangleF rect, float rotation = 0, float opacity = 1)
-        {
-            var textureEntry = this.textureManager.GetTexture(textureKey);
-            if (textureEntry == null)
-            {
-                throw new TextureNotFoundException(textureKey);
-            }
-
-            this.sprite.Begin(SpriteFlags.AlphaBlend);
-           
-            if (rotation == 0.0f)
-            {
-                this.sprite.Draw(textureEntry.Texture, SharpDX.Color.White);
-            }
-            else
-            {
-                var matrix = this.sprite.Transform;
-
-                var center = textureEntry.Center.ToVector3();
-                var scaling = new Vector2((float)rect.Width / textureEntry.Bitmap.Width, (float)rect.Height / textureEntry.Bitmap.Height);
-                this.sprite.Transform = Matrix.Translation(-center) * Matrix.RotationZ(rotation) * Matrix.Translation(center) * Matrix.Scaling(scaling.X, scaling.Y, 0) * Matrix.Translation(rect.X, rect.Y, 0);
-                this.sprite.Draw(textureEntry.Texture, SharpDX.Color.White);
-
-                this.sprite.Transform = matrix;
-            }
-           
-            this.sprite.End();
-        }
-
-        public Vector2 MessureText(string text, float fontSize = 13, string fontFamily = "Calibri")
-        {
-            var font = this.fontCache.GetOrCreate(fontFamily, fontSize);
-            var rect = font.MeasureText(null, text, FontDrawFlags.Left);
-            return new Vector2(rect.Right - rect.Left, rect.Bottom - rect.Top);
         }
 
         public void DrawCircle(Vector2 center, float radius, Color color, float width = 1.0f)
@@ -186,6 +148,45 @@ namespace Ensage.SDK.Renderer.DX9
         {
             var font = this.fontCache.GetOrCreate(fontFamily, fontSize);
             font.DrawText(null, text, (int)position.X, (int)position.Y, new ColorBGRA(color.R, color.G, color.B, color.A));
+        }
+
+        public void DrawTexture(string textureKey, RectangleF rect, float rotation = 0, float opacity = 1)
+        {
+            var textureEntry = this.textureManager.GetTexture(textureKey);
+            if (textureEntry == null)
+            {
+                throw new TextureNotFoundException(textureKey);
+            }
+
+            this.sprite.Begin(SpriteFlags.AlphaBlend);
+
+            var matrix = this.sprite.Transform;
+            var scaling = new Vector2((float)rect.Width / textureEntry.Bitmap.Width, (float)rect.Height / textureEntry.Bitmap.Height);
+            if (rotation == 0.0f)
+            {
+                this.sprite.Transform = Matrix.Scaling(scaling.X, scaling.Y, 0) * Matrix.Translation(rect.X, rect.Y, 0);
+                this.sprite.Draw(textureEntry.Texture, SharpDX.Color.White);
+            }
+            else
+            {
+                var center = textureEntry.Center.ToVector3();
+                this.sprite.Transform = Matrix.Translation(-center)
+                                        * Matrix.RotationZ(rotation)
+                                        * Matrix.Translation(center)
+                                        * Matrix.Scaling(scaling.X, scaling.Y, 0)
+                                        * Matrix.Translation(rect.X, rect.Y, 0);
+                this.sprite.Draw(textureEntry.Texture, SharpDX.Color.White);
+            }
+
+            this.sprite.Transform = matrix;
+            this.sprite.End();
+        }
+
+        public Vector2 MessureText(string text, float fontSize = 13, string fontFamily = "Calibri")
+        {
+            var font = this.fontCache.GetOrCreate(fontFamily, fontSize);
+            var rect = font.MeasureText(null, text, FontDrawFlags.Left);
+            return new Vector2(rect.Right - rect.Left, rect.Bottom - rect.Top);
         }
 
         private void Dispose(bool disposing)

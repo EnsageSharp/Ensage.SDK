0001ff623464c91c911d92afe95127269326ddae
IdcNoob <zajabli@gmail.com>
2018-06-27 10:05:31 +0300

    disabled sentry capture

--- Logger/SentryTarget.cs
+++ Logger/SentryTarget.cs
@@ -66,7 +66,7 @@ namespace Ensage.SDK.Logger
             }
 
             this.cache.Add(id);
-            this.Client.CaptureAsync(logEvent.Exception);
+            //this.Client.CaptureAsync(logEvent.Exception);
         }
     }
 }

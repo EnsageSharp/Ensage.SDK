a8dd05a21d52849007008e68f3bddf90595343f4
beminee <emirdota2@gmail.com>
2019-05-28 09:26:40 +0300

    Added mars and tinker

---
+++ Abilities/npc_dota_hero_mars/mars_bulwark.cs
@@ -0,0 +1,14 @@
+﻿// <copyright file="mars_bulwark.cs" company="Ensage">
+//    Copyright (c) 2019 Ensage.
+// </copyright>
+
+namespace Ensage.SDK.Abilities.npc_dota_hero_mars
+{
+    public class mars_bulwark : PassiveAbility
+    {
+        public mars_bulwark(Ability ability) 
+            : base(ability)
+        {
+        }
+    }
+}
---
+++ Abilities/npc_dota_hero_mars/mars_gods_rebuke.cs
@@ -0,0 +1,32 @@
+﻿// <copyright file="mars_gods_rebuke.cs" company="Ensage">
+//    Copyright (c) 2019 Ensage.
+// </copyright>
+
+namespace Ensage.SDK.Abilities.npc_dota_hero_mars
+{
+    using Ensage.SDK.Extensions;
+
+    public class mars_gods_rebuke : ConeAbility
+    {
+        public mars_gods_rebuke(Ability ability)
+            : base(ability)
+        {
+        }
+
+        public override float Radius
+        {
+            get
+            {
+                return this.Ability.GetAbilitySpecialData("radius");
+            }
+        }
+
+        public float Angle
+        {
+            get
+            {
+                return this.Ability.GetAbilitySpecialData("angle");
+            }
+        }
+    }
+}

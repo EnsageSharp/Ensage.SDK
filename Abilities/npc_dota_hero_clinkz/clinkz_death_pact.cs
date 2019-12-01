a37f1332f0bbcec8e09b2b03d970546219de4954
Jerffelly <30335051+MC01DA@users.noreply.github.com>
2019-12-01 09:59:19 +0200

    Sync
    Clinkz: Removed Strafe, Added Death Pact

---
+++ Abilities/npc_dota_hero_clinkz/clinkz_death_pact.cs
@@ -0,0 +1,18 @@
+﻿// <copyright file="clinkz_death_pact.cs" company="Ensage">
+//    Copyright (c) 2019 Ensage.
+// </copyright>
+
+namespace Ensage.SDK.Abilities.npc_dota_hero_clinkz
+{
+    using Ensage.SDK.Abilities.Components;
+
+    public class clinkz_death_pact : RangedAbility, IHasModifier
+    {
+        public clinkz_death_pact(Ability ability)
+            : base(ability)
+        {
+        }
+
+        public string ModifierName { get; } = "modifier_clinkz_death_pact";
+    }
+}
